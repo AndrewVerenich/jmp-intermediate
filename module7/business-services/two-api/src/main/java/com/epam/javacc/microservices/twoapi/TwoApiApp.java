6bb7a7ef43728e042456caf0c4da3d889a6722d8
Andrei Viarenich <andrei_viarenich@epam.com>
2021-06-17 14:26:26 +0300

    Completed module7

---
+++ module7/business-services/two-api/src/main/java/com/epam/javacc/microservices/twoapi/TwoApiApp.java
@@ -0,0 +1,11 @@
+package com.epam.javacc.microservices.twoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TwoApiApp.class);
+    }
+}
