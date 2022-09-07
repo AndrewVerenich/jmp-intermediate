2b83af6a31c53a82fbac93df78da22433727d717
Andrei Viarenich <andrei_viarenich@epam.com>
2022-09-07 16:50:57 +0300

    Protocol Buffers
    Signed-off-by: AndreyVerenich <andrey.verenich@intradiem.com>

---
+++ protocol-buffers/README.md
@@ -0,0 +1,2 @@
+1. install protoc (https://github.com/protocolbuffers/protobuf/releases)
+2. generate classes from .proto files(protoc *.proto --java_out=.). It's possible to do this with maven plugin, but there were problems
---
+++ protocol-buffers/src/main/java/com/adnver/protoclbuffers/ProtocolBuffersApp.java
@@ -0,0 +1,13 @@
+package com.adnver.protoclbuffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtocolBuffersApp
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ProtocolBuffersApp.class);
+    }
+}
